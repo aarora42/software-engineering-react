4a287d6bc44ae8981985c8df1c213edc31ff2744
anusha <arora.anusha@northeastern.edu>
2022-02-21 14:40:03 -0500

    new

--- src/mongoose/UserSchema.ts
+++ src/mongoose/UserSchema.ts
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-import Location from "../models/Location";
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
+    username: {type: String, required: true, default: `testusername${Date.now()}`},
+    password: {type: String, required: true, default: `testpassword${Date.now()}`},
     firstName: String,
     lastName: String,
-    email: String,
+    email: {type: String, required: true, default: `testemail${Date.now()}`},
     profilePhoto: String,
     headerImage: String,
     accountType: {type: String, default: 'PERSONAL', enum: ['PERSONAL', 'ACADEMIC', 'PROFESSIONAL']},
@@ -14,6 +13,10 @@ const UserSchema = new mongoose.Schema({
     dateOfBirth: Date,
     joined: {type: Date, default: Date.now},
     //change back to Location later
-    location: Number
+    location: {
+        latitude: Number,
+        longitude: Number
+    },
+    salary: {type: Number, default: 50000}
 }, {collection: 'users'});
 export default UserSchema;
